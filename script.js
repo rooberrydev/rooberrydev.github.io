a9594eec6add662b8cf127b33b82fea9ec58aa41
reubengt <reubengeorgethomas@gmail.com>
2021-06-03 14:40:13 +0100

    update CV

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 document.querySelector("body").scrollIntoView({
   behavior: "auto",
 });
-const random = 0;
+
 let playButton = document.querySelector(".play-button");
 
 playButton.addEventListener("click", (e) => {
   document.querySelector("#projects").scrollIntoView();
   document.querySelector("html").style.overflowY = "scroll";
 });
+
 document.onload = () => {
   if (window.pageYOffset > 0)
     document.querySelector("html").style.overflowY = "scroll";
